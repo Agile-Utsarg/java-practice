d60c4b5311a4d0214735c7d55a36c041fa953039
Agile-Utsarg <utsargv@gmail.com>
2026-06-10 14:40:55 +0530

    initial Java practice

---
+++ Main1.java
@@ -0,0 +1,39 @@
+import java.util.*;
+public class Main1 {
+    /*
+    public static int func(int a) {
+
+        System.out.print(a);
+        return a ;
+    }
+
+    static void main() {
+        Scanner scn = new Scanner(System.in);
+        int x = scn.nextInt();
+        System.out.print(x);
+    }
+     */
+    /*
+    static  int fact(int x) {
+        //introduction to function
+        int rv = 1;
+        for (int i = 1; i <= x; i++) {
+            rv = rv * i;
+        }
+        return rv;
+    }
+
+    static void main() {
+            Scanner scn = new Scanner(System.in);
+            int n = scn.nextInt();
+            int r = scn.nextInt();
+
+            int nfact = fact(n);
+            int nmrfact = fact(n-r);
+
+            int npr = nfact / nmrfact;
+            System.out.println(n + "p" + r + "=" + npr);
+        }
+
+     */
+    }
