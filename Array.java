02dd19373f29b87c637a82db83ee95dbfbcce32a
Agile-Utsarg <utsargv@gmail.com>
2026-06-20 15:11:47 +0530

    initial Java practice

---
+++ Array.java
@@ -0,0 +1,16 @@
+void main(){
+ int[] arr; //array decalration
+    //array length
+    arr = new int[5];
+    arr[0] = 33;
+    arr[1] = 35;
+    arr[2] = 57;
+    arr[3] = 76;
+    arr[4] = 12;
+
+    //System.out.println(arr.length);
+    for(int i =0; i < arr.length; i++){
+        System.out.println(arr[i]);
+    }
+
+}
